760477b6c06bc97a587e965d2eec4a474c8ce43c
Pawan Gujral <gujral87@gmail.com>
2021-04-25 20:32:58 +0530

    Add localStorage helper function

---
+++ src/utils/storeHelper.ts
@@ -0,0 +1,39 @@
+export function removeStorageType(type: "local"| "session", key: string) {
+    switch(type) {
+        case "local": 
+            localStorage.removeItem(key);
+            break;
+        case "session": 
+            sessionStorage.removeItem(key);
+            break;
+        default:
+            return false;
+    }
+
+}
+
+export function getStorageType(type: "local"| "session", key: string) {
+    switch(type) {
+        case "local": 
+            localStorage.getItem(key);
+            break;
+        case "session": 
+            sessionStorage.getItem(key);
+            break;
+        default:
+            return false;
+    }
+}
+
+export function setStorageType(type: "local"| "session", key: string, data: string) {
+    switch(type) {
+        case "local": 
+            localStorage.setItem(key, data);
+            break;
+        case "session": 
+            sessionStorage.setItem(key, data);
+            break;
+        default:
+            break;
+    }
+} 
